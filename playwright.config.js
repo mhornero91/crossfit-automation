f0b9dd9f5a90eb133889f04bc971f22aa649400d
Francis <80817164+alvarezfr@users.noreply.github.com>
2022-11-09 21:03:31 +0100

    wip

--- playwright.config.js
+++ playwright.config.js
@@ -51,7 +51,7 @@ const config = {
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-        locale: '[es-ES,es;q=0.9,en-US;q=0.8,en;q=0.7',
+        locale: 'es-ES,es;q=0.9,en-US;q=0.8,en;q=0.7',
         timezoneId: 'Europe/Madrid'
       },
     },
